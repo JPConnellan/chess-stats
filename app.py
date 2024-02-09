dc8196a884242d11683896a73b37af7260bd08ed
DEBJIT <112118014@nitt.edu>
2024-02-09 22:49:55 +0530

    fetches json data and works in streamlit

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import requests
+import streamlit as st
 
+st.set_page_config(page_title="Chess.com Profile",page_icon="♟",
+                    layout="centered",
+                    initial_sidebar_state="collapsed")
+st.header("Chess Insights ♟")
 
-while True:
-    profile = input("Enter your chess.com username: ")
+profile = st.text_input("Enter your chess.com username: ")
 
-    API_URL= "https://api.chess.com/pub/player/"+profile
+API_URL= "https://api.chess.com/pub/player/"+profile
 
-    response = requests.get(API_URL, headers={"User-Agent": "karmadebjit2@gmail.com"})
+submit = st.button('Get Profile')
 
-    print(response.json())
+if submit:
+    response = requests.get(API_URL, headers={"User-Agent": "karmadebjit@gmail.com"})
+    st.write(response.json())
