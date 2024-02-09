1c699f167dd7f42890ea7bf9d5812e959c6e314e
DEBJIT <112118014@nitt.edu>
2024-02-10 00:23:20 +0530

    added error handling scenarios

--- app.py
+++ app.py
@@ -19,16 +19,25 @@ if submit:
     stats = requests.get(STATS_URL, headers={"User-Agent": "karmadebjit@gmail.com"})
     resp_dict = response.json()
     stats_dict = stats.json()
-    st.subheader("Profile for "+resp_dict['name'])
+    if resp_dict.get('name') is None:
+        st.subheader("Player's Profile")
+    else:
+        st.subheader("Profile for "+resp_dict['username'])
     col1,col2,col3,col4,col5,col6 = st.columns([7,10,10,7,7,7])
-    print(resp_dict)
+    print(stats_dict)
     #st.write(stats_dict)
     with col1:
         st.write("Avatar")
-        st.image(resp_dict['avatar'],width=70)
+        if resp_dict.get('avatar') is None:
+            st.write("N/A")
+        else:
+            st.image(resp_dict.get('avatar'),width=70)
     with col2:
         st.write("Name")
-        st.write(resp_dict['name'])
+        if resp_dict.get('name') is None:
+            st.write("N/A")
+        else:
+            st.write(resp_dict['name'])
     with col3:
         st.write("User ID")
         st.write(resp_dict['username'])
@@ -37,8 +46,20 @@ if submit:
         st.write(resp_dict['followers'])
     with col5:
         st.write("Blitz Rating")
-        st.write(stats_dict['chess_blitz']['best']['rating'])
+        if 'chess_blitz' in stats_dict and stats_dict['chess_blitz'] is not None and stats_dict['chess_blitz']['best'] is not None:
+            st.write(stats_dict['chess_blitz']['best']['rating'])
+        elif 'chess_blitz' in stats_dict and stats_dict['chess_blitz'] is not None and stats_dict['chess_blitz']['last'] is not None:
+            st.write(stats_dict['chess_blitz']['last']['rating'])
+        else:
+            st.write("N/A")
     with col6:
         st.write("Rapid Rating")
-        st.write(stats_dict['chess_rapid']['best']['rating'])
+        if 'chess_rapid' in stats_dict and stats_dict['chess_rapid'] is not None and stats_dict['chess_rapid']['best'] is not None:
+            st.write(stats_dict['chess_rapid']['best']['rating'])
+        elif 'chess_rapid' in stats_dict and stats_dict['chess_rapid'] is not None and stats_dict['chess_rapid']['last'] is not None:
+            st.write(stats_dict['chess_rapid']['last']['rating'])
+        else:
+            st.write("N/A")
+
+
 
